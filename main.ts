61ad04138cca6487e9e1be2c634721f8fa47147c
Boyd-Randall <111801637+Boyd-Randall@users.noreply.github.com>
2022-08-22 14:23:37 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+})
 basic.showString("Howdy")
 basic.forever(function () {
     basic.showLeds(`
