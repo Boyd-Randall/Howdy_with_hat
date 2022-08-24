58d183b2d6a6aa05f5b4e46f5779890fded4da79
Boyd-Randall <111801637+Boyd-Randall@users.noreply.github.com>
2022-08-24 12:17:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    spinarrow()
+    while (true) {
+        spinarrow()
+    }
 })
 function spinarrow () {
+    basic.pause(50)
     basic.showArrow(ArrowNames.North)
+    basic.pause(50)
     basic.showArrow(ArrowNames.East)
+    basic.pause(50)
     basic.showArrow(ArrowNames.South)
+    basic.pause(50)
     basic.showArrow(ArrowNames.West)
 }
 input.onButtonPressed(Button.B, function () {
@@ -16,7 +22,8 @@ input.onButtonPressed(Button.B, function () {
         . . . . .
         `)
 })
-basic.showString("Howdy")
+led.setBrightness(50)
+basic.showString(";)")
 basic.forever(function () {
 	
 })
